6f0705d3c336655c787b1d3043b5056d9a86c2e5
Erick Tepan <ert224@lehigh.edu>
2024-07-12 11:08:27 -0400

    request

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config(); // Load environment variables from .env file
 
+const Product = require('./models/product.model.js')
 const MONGODB = process.env.MONGODB;
 
 app.listen(3000, () => {
@@ -13,6 +14,30 @@ mongoose.connect(MONGODB)
     .then(() => console.log('Connected!'))
     .catch(err => console.log('Failed to connect to MongoDB', err));
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
+app.use(express.json())
+
+app.post('/api/products', async (req, res) => {
+    console.log(req.body);
+    try{
+        const product = await Product.create(req.body)
+        res.status(200).json (product)
+    }catch(error){
+        res.status(500).json({
+            message: error.message
+        })
+
+    }
+});
+
+app.get('/api/products', async (req, res) => {
+    console.log(req.body);
+    try{
+        const product = await Product.find({})
+        res.status(200).json (product)
+    }catch(error){
+        res.status(500).json({
+            message: error.message
+        })
+
+    }
 });
